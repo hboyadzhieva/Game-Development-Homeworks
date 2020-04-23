75a3bd18dad49d65c1041aad64a504bc777320cc
Hristina Boyadzhieva <hristinaboyadzhieva@gmail.com>
2020-04-23 22:22:53 +0300

    added Homework 5

---
+++ Homework 5/Assets/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fillIn;
+
+    private void Start()
+    {
+        slider.value = 100;
+        fillIn.color = gradient.Evaluate(1f);
+    }
+
+    public void setHealth(int health) {
+        slider.value = health;
+        fillIn.color = gradient.Evaluate(health * 1f / 100);
+    }
+}
